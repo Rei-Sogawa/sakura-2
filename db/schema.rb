b90983f1560ada5f0e0749f1dd61c09619acc0b6
Rei-Sogawa <natto.and.eggs@gmail.com>
2021-11-21 17:15:04 +0900

    feat: cart_products table に cart_id と product_id の複合ユニークキー制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_21_081026) do
+ActiveRecord::Schema.define(version: 2021_11_21_081245) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_11_21_081026) do
     t.bigint "product_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["cart_id", "product_id"], name: "index_cart_products_on_cart_id_and_product_id", unique: true
     t.index ["cart_id"], name: "index_cart_products_on_cart_id"
     t.index ["product_id"], name: "index_cart_products_on_product_id"
   end
