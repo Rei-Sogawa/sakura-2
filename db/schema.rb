82500cadbb338ccca342023747b5123003750a1e
Rei-Sogawa <natto.and.eggs@gmail.com>
2021-11-21 17:12:19 +0900

    feat: carts table の user_id にユニーク制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_21_025524) do
+ActiveRecord::Schema.define(version: 2021_11_21_081026) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2021_11_21_025524) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_carts_on_user_id"
+    t.index ["user_id"], name: "index_carts_on_user_id", unique: true
   end
 
   create_table "order_products", force: :cascade do |t|
