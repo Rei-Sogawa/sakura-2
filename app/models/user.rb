e91085b6fe7b1a37b9512b69c83863ee1e87f39b
Rei-Sogawa <natto.and.eggs@gmail.com>
2021-11-23 21:56:38 +0900

    feat: devise のデフォルト属性以外の User の属性の登録・更新をできるように

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
   has_one :cart, dependent: :destroy
   has_many :orders, dependent: :destroy
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :address, presence: true
+  validates :postal_code, presence: true
 end
